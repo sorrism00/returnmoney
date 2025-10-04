bca6c820140a9d34b0e1620663e0ae377fbb2dec
sorrism00 <sorrism@naver.com>
2025-10-04 14:49:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,8 @@ self.addEventListener('install', (event) => {
         // 'https://unpkg.com/react@18.2.0/umd/react.production.min.js', // CORS 문제 우회를 위해 제거
         // 'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js', // CORS 문제 우회를 위해 제거
         // 'https://cdn.tailwindcss.com', // 이전에 제거됨
-        'https://placehold.co/180x180/0f172a/fff?text=CC' // PWA 아이콘도 캐시
+        '/returnmoney/icon-192x192.png', // 새로 추가된 아이콘 캐시
+        '/returnmoney/icon-512x512.png'  // 새로 추가된 아이콘 캐시
       ]);
     })
   );
