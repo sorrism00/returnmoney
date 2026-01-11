b3125fca008ee9db64e69b36cfb310e4d734281a
sorrism00 <sorrism@naver.com>
2026-01-11 10:22:32 +0900

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,21 +1,35 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('currency-calculator-v1').then((cache) => {
+    // 캐시 이름 변경: currency-calculator-v1 -> currency-calculator-v2
+    // 버전을 올려야 아이폰이 변경된 index.html을 새로 내려받습니다.
+    caches.open('currency-calculator-v2').then((cache) => {
       return cache.addAll([
-        '/returnmoney/', // PWA 시작 URL
+        '/returnmoney/',
         '/returnmoney/index.html',
         '/returnmoney/manifest.json',
-        '/returnmoney/sw.js', // Service Worker 스크립트 자체도 캐시
+        '/returnmoney/sw.js',
         'https://unpkg.com/@babel/standalone/babel.min.js',
-        // 'https://unpkg.com/react@18.2.0/umd/react.production.min.js', // CORS 문제 우회를 위해 제거
-        // 'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js', // CORS 문제 우회를 위해 제거
-        // 'https://cdn.tailwindcss.com', // 이전에 제거됨
-        '/returnmoney/icon-192x192.png', // 새로 추가된 아이콘 캐시
-        '/returnmoney/icon-512x512.png'  // 새로 추가된 아이콘 캐시
+        '/returnmoney/icon-192x192.png',
+        '/returnmoney/icon-512x512.png'
       ]);
     })
   );
   self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['currency-calculator-v2'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
