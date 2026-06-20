a019372a8dcc8b6e5fb245c5b9383c0697bad679
sorrism00 <sorrism@naver.com>
2026-06-20 14:42:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,55 @@
+const CACHE_NAME = 'currency-calculator-v40';
+const URLS_TO_CACHE = [
+  '/returnmoney/',
+  '/returnmoney/index.html',
+  '/returnmoney/manifest.json',
+  '/returnmoney/sw.js',
+  '/returnmoney/icon-192x192.png',
+  '/returnmoney/icon-512x512.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.12/babel.min.js'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    // 캐시 명칭 버전을 v38로 상승시켜 단말기 캐시 완전 무효화
-    caches.open('currency-calculator-v38').then((cache) => {
-      return cache.addAll([
-        '/returnmoney/',
-        '/returnmoney/index.html',
-        '/returnmoney/manifest.json',
-        '/returnmoney/sw.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.12/babel.min.js',
-        '/returnmoney/icon-192x192.png',
-        '/returnmoney/icon-512x512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      // 에러가 나도 설치를 중단하지 않도록 catch 처리
+      return Promise.allSettled(URLS_TO_CACHE.map(url => cache.add(url)));
     })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['currency-calculator-v38'];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
+  self.clients.claim();
 });
 
+// Network First 전략 (네트워크 연결 시 무조건 최신코드 로드, 실패 시 캐시 사용)
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
